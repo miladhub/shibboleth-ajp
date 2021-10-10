d03a8ca689e056ba97c90054af5bcd0150ff04b9
Milad Bourhani <milad.bourhani@gmail.com>
2021-10-10 14:37:18 +0200

    parsed multi-valued attribute

--- src/main/java/InfoServlet.java
+++ src/main/java/InfoServlet.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = "/info")
 public class InfoServlet extends HttpServlet {
@@ -11,7 +13,11 @@ public class InfoServlet extends HttpServlet {
             throws IOException {
         PrintWriter writer = response.getWriter();
         String uid = "<h2>uid = " + request.getAttribute("uid") + "</h2>";
-        String eduPersonAffiliation = "<h2>eduPersonAffiliation = " + request.getAttribute("eduPersonAffiliation") + "</h2>";
+        String eduAttribute = (String) request.getAttribute("eduPersonAffiliation");
+        if (eduAttribute != null && !eduAttribute.isEmpty())
+            eduAttribute = String.join(", ",
+                    Arrays.asList(eduAttribute.split(";")));
+        String eduPersonAffiliation = "<h2>eduPersonAffiliation = " + eduAttribute + "</h2>";
         String email = "<h2>email = " + request.getAttribute("email") + "</h2>";
         String html = "<html>" + uid + eduPersonAffiliation + email + "</html>";
         writer.println(html);
